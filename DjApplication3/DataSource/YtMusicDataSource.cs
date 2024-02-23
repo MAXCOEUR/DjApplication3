50b017f9d5b2f7146f379703f7e6e5e7dbf91dbf
Maxence COEUR <maxence.chachane@gmail.com>
2024-02-23 20:58:43 +0100

    fix json

--- DjApplication3/DataSource/YtMusicDataSource.cs
+++ DjApplication3/DataSource/YtMusicDataSource.cs
@@ -5,16 +5,12 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using YoutubeExplode;
-using YoutubeExplode.Search;
 using YoutubeExplode.Videos.Streams;
 
 namespace DjApplication3.DataSource
--- DjApplication3/model/SettingsManager.cs
+++ DjApplication3/model/SettingsManager.cs
@@ -1,10 +1,6 @@
 ﻿using CSCore.CoreAudioAPI;
 using NAudio.Midi;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace DjApplication3.model
 {
@@ -27,32 +23,6 @@ namespace DjApplication3.model
         public int browser { get; set; }
         public string browserName { get; set; }
 
-        private string _pathTShark;
-
-        public string pathTShark
-        {
-            get
-            {
-                if (File.Exists("settings.json"))
-                {
-                    // Charger la valeur depuis le fichier JSON
-                    string json = File.ReadAllText("settings.json");
-                    JObject settingsJson = JObject.Parse(json);
-                    string loadedSettings = (string)settingsJson["pathTShark"];
-                    return loadedSettings ?? "";
-                }
-
-                return "C:\\Program Files\\Wireshark\\tshark.exe";
-            }
-            set
-            {
-                // Mettre à jour la valeur et sauvegarder dans le fichier JSON
-                _pathTShark = value;
-                SaveSettings();
-            }
-        }
-        public string numeroUSB { get; set; }
-
         MMDeviceEnumerator enumerator;
         public MMDeviceCollection dispositifsAudio;
 
@@ -69,16 +39,6 @@ namespace DjApplication3.model
             browser = 0;
             browserName = "edge";
             nbrMidi = 0;
-            numeroUSB = "";
-        }
-
-        private void SaveSettings()
-        {
-            // Sérialiser l'objet Settings en format JSON
-            string json = JsonConvert.SerializeObject(new { pathTShark = _pathTShark }, Formatting.Indented);
-
-            // Écrire le JSON dans un fichier
-            File.WriteAllText("settings.json", json);
         }
 
         // Méthode pour obtenir l'instance unique de la classe
