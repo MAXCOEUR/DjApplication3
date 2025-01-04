5d8931b11533285b3092751362b880bff23ea3b9
Maxence COEUR <maxence.chachane@gmail.com>
2025-01-04 15:42:24 +0100

    recherche syncrone bien que la dernier qui sort

--- DjApplication3/view/fragment/ExplorateurYoutubeViewModel.cs
+++ DjApplication3/view/fragment/ExplorateurYoutubeViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DjApplication3.View.userControlDJ
@@ -14,11 +15,23 @@ namespace DjApplication3.View.userControlDJ
         public override event EventHandler<List<Musique>?> TacheSearch;
         public override event EventHandler<Musique?> TacheDownload;
 
+        private CancellationTokenSource _cancellationTokenSearch;
+
         public async override void search(string search)
         {
+            // Annule la recherche précédente si elle est en cours
+            _cancellationTokenSearch?.Cancel();
+            _cancellationTokenSearch = new CancellationTokenSource();
+            var token = _cancellationTokenSearch.Token;
+
             MusiqueRepository musiqueRepository = new MusiqueRepository();
-            List<Musique>? musiques = await Task.Run(() => musiqueRepository.GetMusiqueYoutube(search));
-            TacheSearch?.Invoke(this, musiques);
+            List<Musique>? musiques = await Task.Run(() => musiqueRepository.GetMusiqueYoutube(search), token);
+
+            if (!token.IsCancellationRequested)
+            {
+                TacheSearch?.Invoke(this, musiques);
+            }
+            _cancellationTokenSearch = null;
         }
         public override int? getBpm(Musique musique)
         {
--- DjApplication3/view/fragment/ExplorateurYtMusicViewModel.cs
+++ DjApplication3/view/fragment/ExplorateurYtMusicViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DjApplication3.view.fragment
@@ -15,6 +16,10 @@ namespace DjApplication3.view.fragment
 
         public event EventHandler<List<Musique>?> TacheGetMusiqueInPlayListe;
         public event EventHandler<List<PlayListe>?> TacheGetPlayListe;
+
+        private CancellationTokenSource _cancellationTokenSearch;
+        private CancellationTokenSource _cancellationTokenGetPlaylist;
+        private CancellationTokenSource _cancellationTokenGetPlaylistIn;
 
         public async override void DownloadMusique(Musique musiqueyt)
         {
@@ -31,22 +36,49 @@ namespace DjApplication3.view.fragment
 
         public async override void search(string search)
         {
+            // Annule la recherche précédente si elle est en cours
+            _cancellationTokenSearch?.Cancel();
+            _cancellationTokenSearch = new CancellationTokenSource();
+            var token = _cancellationTokenSearch.Token;
+
             MusiqueRepository musiqueRepository = new MusiqueRepository();
             List<Musique>? musiques = await Task.Run(() => musiqueRepository.GetMusiqueYtMusic(search));
-            TacheSearch?.Invoke(this, musiques);
+
+            if (!token.IsCancellationRequested)
+            {
+                TacheSearch?.Invoke(this, musiques);
+            }
         }
 
         public async void getMusiqueInPlayListe(string idPlayliste)
         {
+            // Annule la recherche précédente si elle est en cours
+            _cancellationTokenGetPlaylistIn?.Cancel();
+            _cancellationTokenGetPlaylistIn = new CancellationTokenSource();
+            var token = _cancellationTokenGetPlaylistIn.Token;
+
             MusiqueRepository musiqueRepository = new MusiqueRepository();
             List<Musique>? musiques = await Task.Run(() => musiqueRepository.GetMusiqueInPlayListeYtMusic(idPlayliste));
-            TacheGetMusiqueInPlayListe?.Invoke(this, musiques);
+
+            if (!token.IsCancellationRequested)
+            {
+                TacheGetMusiqueInPlayListe?.Invoke(this, musiques);
+            }
         }
         public async void getPlayListe()
         {
+            // Annule la recherche précédente si elle est en cours
+            _cancellationTokenGetPlaylist?.Cancel();
+            _cancellationTokenGetPlaylist = new CancellationTokenSource();
+            var token = _cancellationTokenGetPlaylist.Token;
+
             MusiqueRepository musiqueRepository = new MusiqueRepository();
             List<PlayListe>? playListe = await Task.Run(() => musiqueRepository.GetPlayListeYtMusic());
-            TacheGetPlayListe?.Invoke(this, playListe);
+
+            if (!token.IsCancellationRequested)
+            {
+                TacheGetPlayListe?.Invoke(this, playListe);
+            }
         }
     }
 }
